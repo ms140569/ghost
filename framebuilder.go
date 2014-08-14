93e3b6b0088a573853187cc4209f467810d8b09e
d0e00791 <matthias.schmidt@dm.de>
2014-08-14 16:41:36 +0200

    Now the state machine runs.

--- framebuilder.go
+++ framebuilder.go
@@ -4,19 +4,24 @@ import (
 	"log"
 )
 
+// see: go/src/pkg/text/template/parse/lex.go
+// or: http://rspace.googlecode.com/hg/slide/lex.html#landing-slide
+
 type parser struct {
-	pos          int
-	state        stateFn
-	tokens       *[]Token
-	currentFrame *Frame
+	pos    int
+	state  stateFn
+	tokens *[]Token
+	frames *[]Frame
 }
 
 type stateFn func(*parser) stateFn
 
-/*
 func (p *parser) next() Token {
-	return *(p.tokens)[p.pos]
-} */
+	localTokens := *p.tokens
+	tok := localTokens[p.pos]
+	p.pos = p.pos + 1
+	return tok
+}
 
 func (p *parser) run() {
 	for state := startState; state != nil; {
@@ -26,16 +31,19 @@ func (p *parser) run() {
 
 func startState(p *parser) stateFn {
 	log.Println("In startState")
+	log.Printf("Next Token would be: %s", p.next())
 	return getCommandState
 }
 
 func getCommandState(p *parser) stateFn {
 	log.Println("In getCommandState")
+	log.Printf("Next Token would be: %s", p.next())
 	return getHeadersState
 }
 
 func getHeadersState(p *parser) stateFn {
 	log.Println("In getHeadersState")
+	log.Printf("Next Token would be: %s", p.next())
 	return nil
 }
 
