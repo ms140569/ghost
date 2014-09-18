f560ef1b2659bee68a5fc2917718e033e6fef0e6
Matthias Schmidt <matthias.schmidt@gmail.com>
2014-09-18 11:11:47 +0200

    Removing potential bug and exiting with proper unix return code.

--- test/run.sh
+++ test/run.sh
@@ -6,27 +6,27 @@ protocolTests=(
     ["connect-and-send"]=0
 	["connect-and-send-with-receipt"]=0
 	["connect"]=0
-	["connect-too-many-headers"]=-1
-	["connect-with-data"]=-1
-	["connect-without-both-mandatory"]=-1
-	["connect-header-key-too-long"]=-1
-	["connect-header-val-too-long"]=-1
+	["connect-too-many-headers"]=1
+	["connect-with-data"]=1
+	["connect-without-both-mandatory"]=1
+	["connect-header-key-too-long"]=1
+	["connect-header-val-too-long"]=1
 	["connect-disconnect"]=0
-	["connect-disconnect-send"]=-1
-	["garbage10k"]=-1   
-	["garbage"]=-1
+	["connect-disconnect-send"]=1
+	["garbage10k"]=1   
+	["garbage"]=1
 	["stomp"]=0
-	["send-with-receipt"]=-1
-	["stomp-with-data"]=-1
-	["header-corrupt1"]=-1 
-	["invalid-message"]=-1
+	["send-with-receipt"]=1
+	["stomp-with-data"]=1
+	["header-corrupt1"]=1 
+	["invalid-message"]=1
 	["plain-connect"]=0
 )
 
 runTest() {
     # cat $1 |nc -q 1 localhost $PORT|grep -q ^$2 && return 0
     echo $1 $2
-    ./ghostd -f test/$1
+    ../ghostd -f $1 2>/dev/null 1>/dev/null
     return 0
 }
 
