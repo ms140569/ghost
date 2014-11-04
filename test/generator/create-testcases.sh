04f4d1ce01c606539307a3570403de9409c62d71
Matthias Schmidt <matthias.schmidt@gmail.com>
2014-11-04 15:45:32 +0100

    Adding test data generator

---
+++ test/generator/create-testcases.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+RANGE='0 2'
+
+printCommand() {
+    echo CONNECT
+}
+
+printHeader() {
+    for i in `seq 1 $1` 
+    do
+        echo header$i:value$i
+    done
+}
+
+printBody() {
+    for i in `seq 1 $1` 
+    do
+        echo -n B
+    done
+}
+
+printTrailingEOF() {
+    for i in `seq 1 $1` 
+    do
+        echo
+    done
+}
+
+printFrame() {
+    printCommand
+    printHeader $1
+    echo 
+    printBody $2
+    printf '\00'
+    printTrailingEOF $3
+}
+
+PREFIX=tc-conn
+
+for a in `seq $RANGE` 
+do
+	for b in `seq $RANGE` 
+    do
+	    for c in `seq $RANGE` 
+        do
+            # echo $PREFIX-$a-$b-$c.stomp
+            printFrame $a $b $c > $PREFIX-$a-$b-$c.stomp
+            # echo ---------------------------------
+	        
+        done
+
+    done
+
+done
+
