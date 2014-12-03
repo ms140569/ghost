f680abfb8e3e3819688b6f015c493687757a9c41
Matthias Schmidt <matthias.schmidt@gmail.com>
2014-12-03 14:06:59 +0100

    Way better testing for command parsing. Test comes first. Hear me.

--- server/admin/admin_test.go
+++ server/admin/admin_test.go
@@ -4,23 +4,53 @@ import (
 	"testing"
 )
 
-func TestCommandParsing(t *testing.T) {
-	// CommandScanner(content [] byte) []Shellcommand
+func TestCommandParsingGood(t *testing.T) {
 
-	input := "is nix"
+	// single
 
-	CommandScanner([]byte(input))
+	verify(CommandScanner(wrap("status")))
+	verify(CommandScanner(wrap("quit")))
+	verify(CommandScanner(wrap("help")))
 
-	/*
-		if commandsParsed != nil {
-			t.Fatalf("This should produce *no* result: %s", input)
-		} */
+	// single + param
 
-	CommandScanner([]byte("status\n"))
-	CommandScanner([]byte("dest\n"))
-	CommandScanner([]byte("dest gonzo\n"))
-	CommandScanner([]byte("dest list\n"))
-	CommandScanner([]byte("show something\n"))
-	CommandScanner([]byte("quit\n"))
-	CommandScanner([]byte("quit param\n"))
+	verify(CommandScanner(wrap("show log")))
+
+	// dest + sub
+
+	verify(CommandScanner(wrap("dest list")))
+
+	// dest + sub + param
+
+	verify(CommandScanner(wrap("dest create gonzo")))
+	verify(CommandScanner(wrap("dest delete gonzo")))
+	verify(CommandScanner(wrap("dest stat gonzo")))
+
+}
+
+func TestCommandParsingBad(t *testing.T) {
+	// NEGATIVE
+
+	falsify(CommandScanner(wrap("no_valid_command"))) // garbage
+
+	falsify(CommandScanner(wrap("quit param"))) // no params allowed
+
+	falsify(CommandScanner(wrap("dest"))) // without subcommand
+
+	falsify(CommandScanner(wrap("dest gonzo"))) // unknown subcommand
+
+	falsify(CommandScanner(wrap("dest list something"))) // no params allowed
+
+	falsify(CommandScanner(wrap("show"))) // params missing
+
+}
+
+func wrap(data string) []byte {
+	return []byte(data + "\n")
+}
+
+func verify(token Shellcommand) {
+}
+
+func falsify(token Shellcommand) {
 }
